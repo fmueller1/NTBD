7851fd35ab7e0f04ca1fc67d7e4f291583a99788
Felix <felixmmueller06@gmail.com>
2025-09-19 17:21:06 -0700

    fixes

--- src/main/java/freelook/freelook/CannonMath.java
+++ src/main/java/freelook/freelook/CannonMath.java
@@ -1,27 +1,19 @@
-package freelook.freelook;
+package mathTest.MathTest;
 
 public class CannonMath {
-    final double initialOffsetY = 0.25;
     final double initialOffsetX = 0;
+    final double initialOffsetY = 0.25;
     final double initialOffsetZ = 0;
     final double surfaceOffset = -0.25;
     final double distanceFromSurface = 0.375;
     //non-final variables are defined in the constructor as their values depend on non constants.
     double sign = 0;
     double finalXOffset = 0;
-    double finalZOffset = getFinalZOffset(0);
     double doRotation;
 
     public CannonMath(){
         if(distanceFromSurface > 0) sign=1;
         if(distanceFromSurface < 0) sign=-1;
-
-        if(finalXOffset > 0) doRotation=0;
-        if(finalXOffset < 0) doRotation=1;
-        if(finalXOffset == 0) {
-            if(finalZOffset > 0) doRotation=0;
-            if(finalZOffset < 0) doRotation=1;
-        }
 
         finalXOffset = distanceFromSurface + sign*surfaceOffset + initialOffsetX;
     }
@@ -30,8 +22,14 @@ public class CannonMath {
         return distanceFromSurface*Math.cos(cameraYaw) + initialOffsetY;
     }
 
+    private double getDoRotation(double cameraYaw){
+        if(cameraYaw <= -Math.PI/2) return -1.0;
+        if(cameraYaw >= Math.PI/2) return 1.0;
+        return 0.0;
+    }
+
     public double getNewCameraYaw(double cameraPitch, double cameraYaw){
-        finalZOffset = getFinalZOffset(cameraYaw);
+        doRotation = getDoRotation(cameraYaw);
         double theta = Math.atan((finalXOffset*Math.tan(cameraYaw) - initialOffsetZ)/(distanceFromSurface));
         theta = Math.toDegrees(theta);
         theta += 180d*doRotation;
@@ -40,12 +38,12 @@ public class CannonMath {
     }
 
     public double getNewCameraPitch(double cameraPitch, double cameraYaw){
-        finalZOffset = getFinalZOffset(cameraYaw);
         double theta = finalXOffset*Math.tan(cameraPitch);
-        theta *= Math.abs(1d/Math.cos(getNewCameraYaw(cameraPitch, cameraYaw)));
-        theta -= initialOffsetZ;
-        double denominator = finalXOffset * Math.abs(1d/Math.cos(getNewCameraYaw(cameraPitch, cameraYaw)));
+        theta *= Math.abs(1d/Math.cos(cameraYaw));
+        theta -= initialOffsetY;
+        double denominator = distanceFromSurface * Math.abs(1d/Math.cos(getNewCameraYaw(cameraPitch, cameraYaw)));
         theta /= denominator;
+        System.out.println(theta);
         theta = Math.atan(theta);
         return theta;
     }
